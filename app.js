03263fc981173293bb1b14f28c6bfa5da4c82538
Yontemoon <131310574+Yontemoon@users.noreply.github.com>
2023-08-07 13:51:26 -0700

    Finished refactoring. Backend sucessful

---
+++ app.js
@@ -0,0 +1,25 @@
+const config = require("./utils/config");
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogRouter = require('./controllers/blogs')
+// middleware?
+const logger = require("./utils/logger");
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false)
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB');
+    })
+    .catch((error) => {
+        logger.error('MongoDB connection error', error.message)
+    })
+
+app.use(cors())
+// app.use(express.static('build')) USE THIS WHEN CREATING A BUILD??
+app.use(express.json());
+// middleware?
+app.use("/api/blogs", blogRouter)
+
+module.exports = app;
